3df36dd8c8bf0cb5187e96489406f02c66e5cd78
Naoyamessi10 <nanbeee0306@gmail.com>
2019-01-31 09:58:54 +0900

    モデルの検索範囲修正

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -3,7 +3,7 @@ class WorkTime < ApplicationRecord
   validates :time, presence: true
 
   scope :of_search, ->(user_id, date) { joins(:category).where("work_times.user_id = ? and work_times.created_at LIKE ?", "#{user_id}", "2019-#{date}%")}
-  scope :of_week, ->(year, month, day, user_id, today) { joins(:category).where("work_times.user_id = ? and work_times.created_at between ? and ?", "#{user_id}", "#{year}-#{month}-#{day}", "#{today}") }
+  scope :of_week, ->(year, month, day, user_id, today) { joins(:category).where("work_times.user_id = ? and work_times.created_at between ? and ?", "#{user_id}", "#{year}-#{month}-#{day} 00:00:00", "#{today} 23:59:59") }
   scope :search_same_data, ->(time, created_at, user_id) { where(time: time, created_at: created_at, user_id: user_id) }
   
   class << self
